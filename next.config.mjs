9a5b66a1faac08143843bbbc9fc9d04d4d6ce8db
hyeiis <hyeis0807@gmail.com>
2024-05-02 11:25:42 +0900

    [Feat] webpack 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    // dev 모드에서만 소스 맵 사용
+    if (dev) {
+      config.devtool = 'eval-source-map'
+    }
+  },
+}
 
 export default nextConfig
