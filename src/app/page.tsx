2499b1f1546d1bcb634c2f6c518dbb86a085199d
hyeiis <hyeis0807@gmail.com>
2024-05-14 14:27:51 +0900

    [Fix] 확인 페이지 따로 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
 'use client'
 
-import BoxStore, { BoxStyle } from '@/components/atoms/BoxStore'
-import IssueStore, { IssueStyle } from '@/components/atoms/IssueStore'
-import NavBar from '@/components/atoms/NavBar'
+import Check from './Check'
 
 export default function Home() {
   return (
     <>
-      {/* Tag */}
-      <BoxStore boxStyle={BoxStyle.WAIT_TAG} />
-      <BoxStore boxStyle={BoxStyle.PROCEEDING_TAG} />
-      <BoxStore boxStyle={BoxStyle.DONE_TAG} />
-      {/* Badge */}
-      <BoxStore boxStyle={BoxStyle.BADGE} />
-
-      {/* NavBar */}
-      <NavBar>타이틀입니다.</NavBar>
-
-      {/* Issue */}
-      <IssueStore
-        issueStyle={IssueStyle.ISSUE_TOGGLE}
-        title="이슈 제목 입니다아아ddddddddddddddd"
-        content="ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ안녕난나아여여영여여여여여여여여여어어어어어어엉어어어어어어어"
-      />
+      <Check />
     </>
   )
 }
