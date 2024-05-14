115b83c55fdc6a783fba29ca706b084a9e1b170e
hyeiis <hyeis0807@gmail.com>
2024-05-14 12:35:31 +0900

    [Fix] Issue 토글 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import BoxStore, { BoxStyle } from '@/components/atoms/BoxStore'
 import IssueStore, { IssueStyle } from '@/components/atoms/IssueStore'
 import NavBar from '@/components/atoms/NavBar'
@@ -16,7 +18,11 @@ export default function Home() {
       <NavBar>타이틀입니다.</NavBar>
 
       {/* Issue */}
-      <IssueStore issueStyle={IssueStyle.ISSUE_TOGGLE} />
+      <IssueStore
+        issueStyle={IssueStyle.ISSUE_TOGGLE}
+        title="이슈 제목 입니다아아ddddddddddddddd"
+        content="ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ안녕난나아여여영여여여여여여여여여어어어어어어엉어어어어어어어"
+      />
     </>
   )
 }
--- src/components/atoms/IssueStore.tsx
+++ src/components/atoms/IssueStore.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import '../../styles/issueStyle.scss'
 import Image from 'next/image'
+import React, { useState } from 'react'
 
 export enum IssueStyle {
   ISSUE_TOGGLE = 'ISSUE_TOGGLE',
@@ -7,32 +11,62 @@ export enum IssueStyle {
 
 interface Props {
   issueStyle: IssueStyle
+  title: string
+  content: string
 }
 
-export default function IssueStore({ issueStyle }: Props) {
+export default function IssueStore({ issueStyle, title, content }: Props) {
+  const [isOpen, setIsOpen] = useState(false)
+
   const selectIssue = () => {
     switch (issueStyle) {
       case IssueStyle.ISSUE_TOGGLE:
         return (
-          <div className="w-[343px] h-[64px] flex shirnk-0 justify-between items-center font-Pretendard rounded-[16px] bg-white px-[16px]">
-            <div className="flex">
+          <div
+            className={`w-[343px] ${isOpen ? 'h-auto p-[16px]' : 'h-[64px]'} flex-col shirnk-0 justify-between items-center font-Pretendard rounded-[16px] bg-white px-[16px] `}
+          >
+            <div className="flex h-full">
+              <div className="flex pr-[8px] items-center font-semibold text-gray-900">
+                <Image
+                  src="/icons/danger_circle.svg"
+                  width={32}
+                  height={32}
+                  alt="danger_circle"
+                  className="mr-[8px]"
+                />
+                <span
+                  className={`title ${isOpen ? 'overflow-visible' : 'overflow-hidden'} max-w-[239px]`}
+                >
+                  {title}
+                </span>
+              </div>
               <Image
-                src="/icons/danger_circle.svg"
-                width={32}
-                height={32}
-                alt="danger_circle"
+                src="/icons/arrow_down_large.svg"
+                width={24}
+                height={24}
+                alt="arrow_down_large"
+                className="cursor-pointer"
+                onClick={() => setIsOpen(!isOpen)}
               />
-              이슈 사항 제목 표시
             </div>
-            <Image
-              src="/icons/arrow_down_large.svg"
-              width={24}
-              height={24}
-              alt="arrow_down_large"
-              className="cursor-pointer"
-            />
+            {isOpen && (
+              <>
+                <div className="my-[16px] px-[24px] py-[16px] justify-center items-center gap-[8px] rounded-[16px] bg-gray-100 h-auto font-Pretendard">
+                  {content}
+                </div>
+                <div className="flex justify-between">
+                  <button type="button" className="bg-green-100 w-[150px]">
+                    다시 보지 않음
+                  </button>
+                  <button type="button" className="bg-green-100 w-[150px]">
+                    접어 두기
+                  </button>
+                </div>
+              </>
+            )}
           </div>
         )
+
       default:
         return null
     }
