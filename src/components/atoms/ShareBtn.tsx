dc466c3341d22492cf1742d212d9d87ad68bc159
yerim08 <sbnlover1008@naver.com>
2024-05-16 16:08:05 +0900

    [Fix] shareBtn 스타일 수정

--- src/components/atoms/ShareBtn.tsx
+++ src/components/atoms/ShareBtn.tsx
@@ -5,8 +5,8 @@ import '../../styles/shareStyle.scss'
 
 export default function ShareBtn() {
   return (
-    <button className="box w-[343px] h-[52px] justify-center">
-      <div className="flex  items-center">
+    <button className="box w-[343px] h-[52px] justify-center ">
+      <div className="flex  items-center ">
         <Image
           src="/icons/arrow_forward.svg"
           width={32}
@@ -14,9 +14,9 @@ export default function ShareBtn() {
           alt="arrow_forward"
           className="mr-[8px] w-[24px] h-[24px]"
         />
-        <div className="content">
+        <div className="share-content font-Pretendard leading-6">
           같은 건물 소유자에서
-          <span className="bold"> 사안 공유하기</span>
+          <span className="bold font-Pretendard"> 사안 공유하기</span>
         </div>
       </div>
     </button>
