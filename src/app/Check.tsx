02e5de955a0182bc1e737df9fe2727cc1eb0a710
yerim08 <sbnlover1008@naver.com>
2024-05-16 16:02:23 +0900

    [Feat] 댓글, 진행상황 없는 버튼

--- src/app/Check.tsx
+++ src/app/Check.tsx
@@ -4,6 +4,7 @@ import BoxStore, { BoxStyle } from '@/components/atoms/BoxStore'
 import CardStore, { CardStyle } from '@/components/atoms/CardStore'
 import IssueStore, { IssueStyle } from '@/components/atoms/IssueStore'
 import NavBar from '@/components/atoms/NavBar'
+import NoBox from '@/components/atoms/NoBox'
 import ShareBtn from '@/components/atoms/ShareBtn'
 import TooltipStore, { TooltipStyle } from '@/components/atoms/TooltipStore'
 
@@ -16,11 +17,9 @@ export default function Check() {
       <BoxStore boxStyle={BoxStyle.DONE_TAG} />
       {/* Badge */}
       <BoxStore boxStyle={BoxStyle.BADGE} />
-
       {/* NavBar */}
       <NavBar>타이틀입니다.</NavBar>
       <br />
-
       {/* Issue */}
       <IssueStore
         issueStyle={IssueStyle.ISSUE_TOGGLE}
@@ -34,11 +33,10 @@ export default function Check() {
         content="ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ안녕난나아여여영여여여여여여여여여어어어어어어엉어어어어어어어"
       />
       <br />
-
       {/* Tooltips */}
       <TooltipStore tooltipStyle={TooltipStyle.COUNT} count={10} />
       <TooltipStore tooltipStyle={TooltipStyle.TEXT_CHANGE} />
-
+      <br />
       {/* Card */}
       <CardStore
         cardStyle={CardStyle.ALL}
@@ -50,6 +48,7 @@ export default function Check() {
         view={0}
         comment={0}
       />
+      <br />
       <CardStore
         cardStyle={CardStyle.NO_IMG}
         img_src=""
@@ -60,6 +59,7 @@ export default function Check() {
         view={0}
         comment={0}
       />
+      <br />
       <CardStore
         cardStyle={CardStyle.NO_PERIOD}
         img_src="https://s3-alpha-sig.figma.com/img/1ca6/0f1b/9e8fbefccad6e39d044cbb5cf9c713aa?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cMHMTT7DiwDF5-BUGJhrN3YemZfwfckwHeaBMM0phs7pRTiaWZoxT4EYBe3YV5lMRziHY32oMVqrSBwqk4jyc1tXbeLgfTYzEDqa8zSUoqbAe70hocphLaXcw2e2ipKms8A7xnqebRf34U6kb-m-KC96kWDnGZxzW0hLInWAUtCr8gpRH~19ZPEEATiOSCd6tSMR3lGsMCUDmq4YF~Z~jjmM9p54s1VdNoWHEN3wurAxZN6SItiDkQRYMYdybFinnmXYeiVkg826ZCnFs-oNsEHGjEbGfCvVX52cDaN2clOXHTSEdvHUYz3hnQ~bJEBPSDKPVLMcrDhcchuahxNV0w__"
@@ -70,6 +70,7 @@ export default function Check() {
         view={0}
         comment={0}
       />
+      <br />
       <CardStore
         cardStyle={CardStyle.ALL_NO}
         img_src="https://s3-alpha-sig.figma.com/img/1ca6/0f1b/9e8fbefccad6e39d044cbb5cf9c713aa?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cMHMTT7DiwDF5-BUGJhrN3YemZfwfckwHeaBMM0phs7pRTiaWZoxT4EYBe3YV5lMRziHY32oMVqrSBwqk4jyc1tXbeLgfTYzEDqa8zSUoqbAe70hocphLaXcw2e2ipKms8A7xnqebRf34U6kb-m-KC96kWDnGZxzW0hLInWAUtCr8gpRH~19ZPEEATiOSCd6tSMR3lGsMCUDmq4YF~Z~jjmM9p54s1VdNoWHEN3wurAxZN6SItiDkQRYMYdybFinnmXYeiVkg826ZCnFs-oNsEHGjEbGfCvVX52cDaN2clOXHTSEdvHUYz3hnQ~bJEBPSDKPVLMcrDhcchuahxNV0w__"
@@ -80,8 +81,16 @@ export default function Check() {
         view={0}
         comment={0}
       />
-
+      <br />
+      {/* Share */}
       <ShareBtn />
+      <br />
+
+      {/* NoBox */}
+      <NoBox type="댓글" />
+      <br />
+      <NoBox type="진행 내용" />
+      <br />
     </>
   )
 }
