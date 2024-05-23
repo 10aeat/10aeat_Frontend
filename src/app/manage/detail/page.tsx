7f85b6943c4b358b42f9581ff09fcfdec8bad702
yerim08 <sbnlover1008@naver.com>
2024-05-23 23:33:43 +0900

    [Feat] 법정 시설물 진행 현황 Card

--- src/app/manage/detail/page.tsx
+++ src/app/manage/detail/page.tsx
@@ -5,7 +5,9 @@ import TrackingProgress2 from '@/components/atoms/TrackingProcess2'
 export default function ManageDetail() {
   return (
     <div className="flex flex-col w-full items-center">
-      <TrackingProgress2 />
+      <TrackingProgress2 totalChecks={10} completedChecks={0} status="대기" />
+      <TrackingProgress2 totalChecks={12} completedChecks={3} status="진행중" />
+      <TrackingProgress2 totalChecks={1} completedChecks={1} status="완료" />
     </div>
   )
 }
