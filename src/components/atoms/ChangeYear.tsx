c88b9402bcd1739f1da39e316708fd4e7400e0cc
hyeiis <hyeis0807@gmail.com>
2024-05-26 16:05:14 +0900

    [Fix] manage 페이지 수정

---
+++ src/components/atoms/ChangeYear.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+interface Props {
+  selectedYear: number
+  onPreviousYear: () => void
+  onNextYear: () => void
+}
+
+export default function ChangeYear({
+  selectedYear,
+  onPreviousYear,
+  onNextYear,
+}: Props) {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <>
+      <div className="flex justify-center items-center mt-[10px] mb-4">
+        <Image
+          src="icons/lastyear.svg"
+          width={10}
+          height={10}
+          alt="이전"
+          onClick={onPreviousYear}
+          className="cursor-pointer"
+        />
+        <span className="mx-4 text-gray-700 font-semibold font-Pretendard text-xl capitalize">
+          {selectedYear}
+        </span>
+        <Image
+          src="icons/nextyear.svg"
+          width={10}
+          height={10}
+          alt="다음"
+          onClick={selectedYear === currentYear ? undefined : onNextYear}
+          className={
+            selectedYear === currentYear
+              ? 'opacity-30 disabled'
+              : 'cursor-pointer'
+          }
+        />
+      </div>
+    </>
+  )
+}
---
+++ src/components/atoms/NoData.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function NoData() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-[450px]">
+      <Image
+        src={'/icons/clipboard_check.svg'}
+        width={80}
+        height={80}
+        alt="no-data"
+        className="shrink-0"
+      />
+      <span className="text-gray-600 text-center font-Pretendard text-xl font-bold capitalize">
+        등록된 법정 시설물 <br />
+        유지관리 점검이 없습니다.
+      </span>
+    </div>
+  )
+}
