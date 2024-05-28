7da4019861cb1f180d506e8747e34d7af21563ae
yerim08 <sbnlover1008@naver.com>
2024-05-28 18:28:52 +0900

    [Fix] 하단 내비 스타일 수정

--- src/components/atoms/BottomNav.tsx
+++ src/components/atoms/BottomNav.tsx
@@ -40,7 +40,7 @@ import Image from 'next/image'
 export default function BottomNav() {
   return (
     <div className="fixed bottom-0 w-[375px] h-[56px] rounded-t-[18px] bg-white z-10 flex justify-between">
-      <div className="flex-1 flex items-center justify-center">
+      <button className="flex-1 flex items-center justify-center">
         <div className="relative w-[47px] h-[44px]">
           <Image
             className="!absolute !w-[20px] !h-[20px] !left-[13px]"
@@ -53,8 +53,8 @@ export default function BottomNav() {
             소유자 홈
           </div>
         </div>
-      </div>
-      <div className="flex-1 flex items-center justify-center">
+      </button>
+      <button className="flex-1 flex items-center justify-center">
         <div className="relative w-[54px] h-[44px]">
           <Image
             className="!absolute !w-[24px] !h-[24px] !left-[13px]"
@@ -67,7 +67,7 @@ export default function BottomNav() {
             마이페이지
           </div>
         </div>
-      </div>
+      </button>
     </div>
   )
 }
