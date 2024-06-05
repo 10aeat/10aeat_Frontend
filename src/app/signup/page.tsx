f44d7d65e5dffecd2f74efb9bdd1b2b8b4213457
yerim08 <sbnlover1008@naver.com>
2024-06-05 10:00:43 +0900

    [Fix] 라우터 및 로그인 페이지 기능 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import Image from 'next/image'
 import AdminButton, {
   ButtonStyle,
 } from '../admin/_components/atoms/AdminButton'
+import { useRouter } from 'next/navigation'
 
 export default function page() {
+  const router = useRouter()
   return (
     <div className="flex flex-col w-full items-center bg-white h-[812px]">
       <div className="absolute flex w-[193px] h-[56px] top-[193px] justify-center items-center gap-[4px]">
@@ -26,10 +29,18 @@ export default function page() {
         편리한 오피스 생활의 시작
       </div>
       <div className="absolute top-[446px] flex flex-col w-[343px] p-[14px] gap-[16px]">
-        <AdminButton buttonSize="md" buttonStyle={ButtonStyle.PRIMARY}>
+        <AdminButton
+          buttonSize="md"
+          buttonStyle={ButtonStyle.PRIMARY}
+          onClickFunction={() => router.push('/signin')}
+        >
           로그인
         </AdminButton>
-        <AdminButton buttonSize="md" buttonStyle={ButtonStyle.ACCENT}>
+        <AdminButton
+          buttonSize="md"
+          buttonStyle={ButtonStyle.ACCENT}
+          onClickFunction={() => router.push('/signup/step1')}
+        >
           <Image
             src="/icons/letter_blue.svg"
             width={24}
