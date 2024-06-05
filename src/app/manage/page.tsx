a60c00a3fa592eb3aa1b354f3e0218bf7ecf8669
hyeiis <hyeis0807@gmail.com>
2024-06-05 18:34:35 +0900

    [Fix] eslint 오류 해결

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -64,7 +64,7 @@ export default function ManageList() {
       }
     }
     getManageArticlesList()
-  })
+  }, [])
 
   const handleStatusClick = (status: string) => {
     setSelectedStatus(status)
