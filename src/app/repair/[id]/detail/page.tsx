32417155ce3f4272f033b57e69e3dd587e104de9
hyeiis <hyeis0807@gmail.com>
2024-06-12 12:25:33 +0900

    [Fix] 코드 수정

--- src/app/repair/[id]/detail/page.tsx
+++ src/app/repair/[id]/detail/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import RepairDetailOrganism from '@/components/organisms/RepairDetailOrganism'
-import { useParams } from 'next/navigation'
+import { useParams, useSearchParams } from 'next/navigation'
 
 export default function RepairDetail() {
-  const { id, issueId } = useParams()
+  const { id } = useParams()
+  const searchParams = useSearchParams() // 쿼리 파라미터를 가져옴
 
+  const issueId = searchParams.get('activeIssueId') // 쿼리 파라미터에서 'activeIssueId'를 가져옴
   return (
     <main className="flex flex-col items-center justify-center">
       <RepairDetailOrganism repairArticleId={id} issueId={issueId} />
